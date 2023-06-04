b206328d41aba62370c6358c8a5799ea33ed9074
Sagun Karki <sagunk064@gmail.com>
2023-06-04 11:57:22 -0500

    added all required ev graphs

--- research/publication/ev_adopt_graphs/graphs.py
+++ research/publication/ev_adopt_graphs/graphs.py
@@ -98,7 +98,7 @@ off_road_freq_dict = {
 
 # make this dict more hex
 color_map_off_road_freq = {
-    1.0: '#1f77b4',
+    1.0: 'black',
     2.0: '#2ca02c',
     3.0: '#d62728',
     4.0: '#ff7f0e',
@@ -115,7 +115,7 @@ parking_dict = {
 }
 
 
-def get_next_veh_by_hh_size_graph():
+def get_fig_next_veh_by_hh_size_graph():
     """
     returns graph for Next vehicle choice by household size
     """
@@ -133,7 +133,109 @@ def get_next_veh_by_hh_size_graph():
     result['next_veh_type_1'] = result['next_veh_type_1'].astype(str)
 
     fig = px.bar(result, x="hh_size", y="percentage", color="next_veh_type_1", barmode="stack")
-    fig.update_layout(yaxis=dict(tickformat=".2%"))
+    fig.update_layout(yaxis=dict(tickformat=".0%"))
+
+    fig.update_layout(
+        xaxis=dict(
+            tickmode='linear',
+            dtick=1,
+            tickangle=0,
+            title_text='Household size',
+        ),
+        yaxis=dict(
+            title_text=None,
+
+        ),
+        # height=500,
+        legend=dict(
+            title_text=None  # Remove legend title
+        ),
+    )
+
+    return fig
+
+def get_fig_off_freq_by_occ():
+    df_off_freq_by_occ = geo_edf[['occupation', 'off_road_freq']].copy()
+    df_off_freq_by_occ = df_off_freq_by_occ.sort_values(by=['off_road_freq'])
+    df_off_freq_by_occ.loc[:, 'occupation_legend'] = df_off_freq_by_occ['occupation'].map(occupation_dict)
+    df_off_freq_by_occ.loc[:, 'off_road_freq'] = df_off_freq_by_occ['off_road_freq'].map(off_road_freq_dict)
+
+    # count the number of each off road frequency by occupation
+    result = df_off_freq_by_occ.groupby('occupation_legend')['off_road_freq'].value_counts().reset_index(name='count_each_off_road_freq')
+
+    # calulate the percentage of each off road frequency
+    result['percentage'] = result['count_each_off_road_freq'] / result.groupby('occupation_legend')['count_each_off_road_freq'].transform('sum')
+
+    # convert off_road_freq to string as it is categorical (We need to change this to proper categorical variable later)
+    result['off_road_freq'] = result['off_road_freq'].astype(str)
+
+
+    # plot it in stacked bar chart
+    fig = px.bar(result, x="occupation_legend", y="percentage", color="off_road_freq", barmode="stack")
+
+    fig.update_layout(
+        xaxis=dict(
+            tickmode='linear',
+            dtick=1,
+            tickangle=50,
+            title_text=None
+        ),
+        yaxis=dict(
+            title_text=None
+        ),
+        height=650,
+        title_text="Off-road driving frequency by occupation",
+        legend=dict(
+            title_text=None  # Remove legend title
+        ),
+
+
+    )
+    fig.for_each_trace(lambda t: t.update(name=t.name.replace("\n", "<br>")))
+    fig.update_layout(yaxis=dict(tickformat=".0%"))
+
+    #manually reorder the legend based on frequency
+    data_reordered = [fig.data[0], fig.data[1], fig.data[3], fig.data[2]]
+    fig.data = data_reordered
+
+    return fig
+
+def get_fig_off_road_by_next_veh():
+    df_off_road_next_veh = geo_edf[['next_veh_type_1', 'off_road_freq']].copy()
+    # combine other vehicle to large car
+    df_off_road_next_veh['next_veh_type_1'] = df_off_road_next_veh['next_veh_type_1'].copy().apply(lambda x: 2 if x == 4 else x)
+
+    df_off_road_next_veh.loc[:, 'next_veh_type_1'] = df_off_road_next_veh['next_veh_type_1'].map(next_veh_dict)
+
+    df_off_road_next_veh.loc[:, 'off_road_freq'] = df_off_road_next_veh['off_road_freq'].map(off_road_freq_dict)
+
+    # Define the vehicle order
+    vehicle_order = ["Small car", "Large car", "Pickup truck"]
+
+    # sort the graph in order
+    df_off_road_next_veh_copy = df_off_road_next_veh.copy()
+    df_off_road_next_veh_copy['next_veh_type_1'] = pd.Categorical(df_off_road_next_veh_copy['next_veh_type_1'], categories=vehicle_order, ordered=True)
+    df_off_road_next_veh = df_off_road_next_veh_copy
+
+
+    # count the number of each off road frequency by next vehicle type
+    result = df_off_road_next_veh.groupby('next_veh_type_1')['off_road_freq'].value_counts().reset_index(name='count_each_off_road_freq')
+
+    # calulate the percentage of each off road frequency make it out of 100
+    result['percentage'] = result['count_each_off_road_freq'] / result.groupby('next_veh_type_1')['count_each_off_road_freq'].transform('sum')
+
+    # convert off_road_freq to string as it is categorical (We need to change this to proper categorical variable later)
+    result['off_road_freq'] = result['off_road_freq'].astype(str)
+
+    # result['off_road_freq'] = result['off_road_freq'].map(color_map_off_road_freq)
+
+    # plot it in stacked bar chart
+    fig = px.bar(result, x="next_veh_type_1", y="percentage", color="off_road_freq", barmode="stack")
+
+    # change the percentage to 2 decimal places
+    fig.for_each_trace(lambda t: t.update(name=t.name.replace("\n", "<br>")))
+    fig.update_layout(yaxis=dict(tickformat=".0%"))
+
 
     fig.update_layout(
         xaxis=dict(
@@ -146,10 +248,269 @@ def get_next_veh_by_hh_size_graph():
             title_text=None
         ),
         height=500,
-        title_text="Off-road driving frequency by expected next vehicle type",
         legend=dict(
             title_text=None  # Remove legend title
         ),
     )
+    #manually reorder the legend based on frequency
+    data_reordered = [fig.data[0], fig.data[1], fig.data[3], fig.data[2]]
+    fig.data = data_reordered
+
+    return fig
+
+
+def get_fig_time_fuel_type():
+    df_time_fuel_type = geo_edf[['next_veh', 'next_veh_type_1']].copy()
+    # combine other vehicle to large car
+    df_time_fuel_type['next_veh_type_1'] = df_time_fuel_type['next_veh_type_1'].copy().apply(lambda x: 2 if x == 4 else x)
+
+    df_time_fuel_type.loc[:, 'next_veh_type_1'] = df_time_fuel_type['next_veh_type_1'].map(next_veh_dict)
+    df_time_fuel_type.columns = ['next_veh_purchase_time', 'next_veh_type']
+
+    result = df_time_fuel_type.groupby('next_veh_purchase_time')['next_veh_type'].value_counts().reset_index(name='count_each_fuel_type')
+
+    # convert count to percentage
+    result['percentage'] = result['count_each_fuel_type'] / result.groupby('next_veh_purchase_time')['count_each_fuel_type'].transform('sum')
+
+    # convert next_veh_type to string as it is categorical (We need to change this to proper categorical variable later)
+    result['next_veh_type'] = result['next_veh_type'].astype(str)
+    result['next_veh_purchase_time'] = result['next_veh_purchase_time'].astype(str)
+
+    #show points on line chart
+    fig = px.line(result, x="next_veh_purchase_time", y="percentage", color="next_veh_type", markers=True)
+
+    fig.update_layout(
+    xaxis=dict(
+        tickmode='linear',
+        dtick=1,
+        tickangle=0,
+        title_text = 'Year',
+    ),
+    yaxis=dict(
+        title_text=None
+    ),
+    height=500,
+    legend=dict(
+        title_text=None  # Remove legend title
+    ),
+    )
+    fig.update_layout(yaxis=dict(tickformat=".0%"))
+
+
+    return fig
+
+def get_fig_parking():
+    df_charging_loc = geo_edf[parking_dict.keys()].copy()
+    df_charging_loc.rename(columns=parking_dict, inplace=True)
+
+    # df_charging_loc.astype(int)
+
+    result = pd.DataFrame(df_charging_loc.sum().reset_index(name='count'))
+
+    # change the column name
+    result.columns = ['charging_loc', 'count']
+
+    # make a pie chart
+    fig = px.pie(result, values='count', names='charging_loc', title='Charging location')
+
+    fig.update_layout(
+    xaxis=dict(
+        tickmode='linear',
+        dtick=1,
+        tickangle=0
+    ),
+    height=600,
+    legend=dict(
+        title_text=None  # Remove legend title
+    ),
+    )
+    
+    return fig
+
+
+def prepare_df_sankey(df, source, target):
+    label_list = df[source].unique().tolist() + df[target].unique().tolist()
+
+    df['source_index'] = df[source].copy().apply(lambda x: label_list.index(x))
+    df['target_index'] = df[target].copy().apply(lambda x: label_list.index(x))
+
+    return df, label_list
+
+
+
+def get_fig_sankey_sp_next_veh():
+    df_for_sankey = geo_edf[['veh_pt_1', 'veh_type_1', 'sp1', 'comb_weight']].copy()
+    df_for_sankey['sp1'] = df_for_sankey['sp1'].map({1: "Small ICE", 2: "Small BEV", 3: "Large ICE", 4: "Large BEV", 5:"Pickup truck ICE", 6: "Pickup truck BEV"})
+    df_for_sankey['veh_pt_1'] = df_for_sankey['veh_pt_1'].map({1: "ICE", 2: "BEV", 3: "HEV", 4: "PHEV"})
+    df_for_sankey['veh_type_1'] = df_for_sankey['veh_type_1'].map({1: "Small Car", 2: "Large Car ", 3: "Pickup Truck", 4: "Other"})
+
+
+    # filter anything that is not nan
+    df_for_sankey.fillna(-1, inplace=True)
+    df_for_sankey = df_for_sankey[(df_for_sankey['sp1'] != -1) & (df_for_sankey['veh_type_1'] != -1) & (df_for_sankey['veh_pt_1'] != -1)].copy()
+
+
+    #convert them to int
+    # df_for_sankey[['veh_type_1', 'veh_pt_1', 'sp1']] = df_for_sankey[['veh_type_1', 'veh_pt_1', 'sp1']].astype(int)
+
+    # create a new column that is a combination of veh_pt_1 and veh_type_1
+    df_for_sankey['curr'] = df_for_sankey['veh_pt_1'].astype(str) + '_' + df_for_sankey['veh_type_1'].astype(str)
+    df_for_sankey.sort_values(by=['curr', 'sp1'], inplace=True)
+    df_for_sankey
+
+
+    df_for_sankey = df_for_sankey.groupby(['curr', 'sp1']).sum('comb_weight').reset_index()
+    df_for_sankey.sort_values(['curr', 'sp1'], inplace=True)
+    df_for_sankey
+
+    # Get unique values from 'veh_pt_1' and 'veh_type_1' columns
+    veh_pt_values = np.sort(np.array(geo_edf['veh_pt_1'].unique()))
+    veh_type_values = np.sort(np.array(geo_edf['veh_type_1'].unique()))
+
+    # Create the mapping dictionary using dictionary comprehension
+    mapping_dict = {f'{i+1}_{j+1}': num for num, (i, j) in enumerate(np.ndindex(len(veh_pt_values), len(veh_type_values)), start=1)}
+
+    df_for_sankey['curr_mapping'] = df_for_sankey['curr'].map(mapping_dict)
+    df_for_sankey
+
+    df, label_list = prepare_df_sankey(df_for_sankey, 'curr', 'sp1')
+
+
+    label_list = df_for_sankey['curr'].unique().tolist() + df_for_sankey['sp1'].unique().tolist()
+
+
+    fig = go.Figure(data=[go.Sankey(
+    node=dict(
+        pad=15,
+        thickness=20,
+        line=dict(color="black", width=0.5),
+        label=label_list,
+        # color="blue",
+        x=[0, 0, 0, 1, 1, 1, 1]  # Adjust x-axis position for each node
+    ),
+    link=dict(
+        source=df['source_index'].to_list(),
+        target=df['target_index'].to_list(),
+        value=df['comb_weight'].to_list()
+    )
+    )])
+
+    fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
+
+    return fig
+
+
+def apply_legend_curr_next(df):
+    curr_veh_pt_dict = {1: "ICE", 2: "BEV", 3: "HEV", 4: "PHEV"}
+    curr_veh_type_dict = {1: "Small Car", 2: "Large Car ", 3: "Pickup Truck", 4: "Other"}
+
+    next_veh_pt_dict = {1: "Other", 2: "BEV", 3: "HEV", 4: "PHEV",  5: "ICE",}
+    next_veh_type_dict = {1: "Small Car", 2: "Other", 3: "Pickup Truck", 4: "Large Car"}
+
+    curr_dict = {}
+    for i in curr_veh_pt_dict.keys():
+        for j in curr_veh_type_dict.keys():
+            curr_dict[f"C_{i}_C_{j}"] = f"{curr_veh_pt_dict[i]} {curr_veh_type_dict[j]}"
+
+    next_dict = {}
+    for i in next_veh_pt_dict.keys():
+        for j in next_veh_type_dict.keys():
+            # print(f"C_{i}_C_{j} = {next_veh_pt_dict[i]}__{next_veh_type_dict[j]}")
+            next_dict[f"N_{i}_N_{j}"] = f"{next_veh_pt_dict[i]} {next_veh_type_dict[j]}"
+
+    df.loc[:, 'source_label'] = df['source'].map(curr_dict)
+    df.loc[:, 'target_label'] = df['target'].map(next_dict)
+
+    return df
+
+
+def get_fig_sankey_curr_next():
+
+    df_for_sankey = geo_edf[
+        ['veh_type_1',  'veh_pt_1', 'next_veh_type_1', 'next_veh_pt_1', 'comb_weight']
+    ].copy()
+
+    # filter out the rows that is not -1
+    df_for_sankey.fillna(-1, inplace=True)
+    df_for_sankey = df_for_sankey[
+        (df_for_sankey['veh_type_1'] != -1) & (df_for_sankey['next_veh_type_1'] != -1) & (df_for_sankey['next_veh_pt_1'] != -1) & (df_for_sankey['veh_pt_1'] != -1)
+        ].copy()
+
+    df_for_sankey[['veh_type_1',  'veh_pt_1', 'next_veh_type_1', 'next_veh_pt_1',]] = df_for_sankey[['veh_type_1',  'veh_pt_1', 'next_veh_type_1', 'next_veh_pt_1',]].copy().astype(int)
+
+    df_for_sankey['veh_type_1'] = "C_"+df_for_sankey['veh_type_1'].copy().astype(str) 
+    df_for_sankey['veh_pt_1'] = "C_"+df_for_sankey['veh_pt_1'].copy().astype(str) 
+    df_for_sankey['next_veh_type_1'] = "N_"+df_for_sankey['next_veh_type_1'].copy().astype(str)
+    df_for_sankey['next_veh_pt_1'] = "N_"+df_for_sankey['next_veh_pt_1'].copy().astype(str)
+
+    df_for_sankey['source'] = df_for_sankey['veh_pt_1'] + "_" + df_for_sankey['veh_type_1']
+    df_for_sankey['target'] = df_for_sankey['next_veh_pt_1'] + "_" + df_for_sankey['next_veh_type_1']
+    df_x = df_for_sankey.groupby(['source', 'target']).sum('comb_weight').reset_index()
+
+
+
+    df, label_list = prepare_df_sankey(df_x, 'source', 'target')
+
+
+    diff = 10
+
+    fig = go.Figure()
+
+    for i in range(0, int(df['comb_weight'].max()) + diff, diff):
+        df_x = df[df['comb_weight'] > i].copy()
+        df, label_list = prepare_df_sankey(df_x, 'source', 'target')
+        apply_legend_curr_next(df)
+
+        # values = df['comb_weight'].to_list()
+
+        label_list = df['source_label'].unique().tolist() + df['target_label'].unique().tolist()
+
+        fig.add_trace(
+            go.Sankey(
+                node=dict(
+                    pad=15,
+                    thickness=20,
+                    line=dict(color="black", width=0.5),
+                    label=label_list,
+                    x=[0, 0, 0, 1, 1, 1, 1]  # Adjust x-axis position for each node
+                ),
+                link=dict(
+                    source=df['source_index'].to_list(),
+                    target=df['target_index'].to_list(),
+                    value=df['comb_weight'].to_list()
+                ),
+                visible=False  # Set the visibility of the trace to False initially
+            )
+        )
+
+
+
+    # Create and add slider
+    steps = []
+    for i in range(len(fig.data)-1):
+        step = dict(
+            method="update",
+            args=[{"visible": [False] * len(fig.data)},
+                #   {"title": f"Slider switched to comb weight > {str(i * diff)}"}],  # layout attribute
+            ],
+            label=f" "  # Set custom label for each step
+        )
+        step["args"][0]["visible"][i] = True  # Toggle i'th trace to "visible"
+        steps.append(step)
+
+    n = 2 # slider default value
+    sliders = [dict(
+    active=n,  # Set the default step index
+    currentvalue={"prefix": ""},
+    # pad={"t": 50},
+    steps=steps
+    )]
+
+    fig.update_layout(
+    sliders=sliders
+    )
+
+    fig.data[n].visible = True  # Toggle first trace to "visible"
+    # fig.update_layout(title_text=f"Sankey Diagram: comb weight > {n*diff}", font_size=10)
 
     return fig
